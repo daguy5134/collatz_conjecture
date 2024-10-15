39273f6c659a884c9288d0264568737d93f6a5b5
daguy5134 <150766542+daguy5134@users.noreply.github.com>
2024-10-15 17:24:57 +0800

    program_2.1.py

--- program.py
+++ program.py
@@ -2,15 +2,16 @@ true_false2 = False
 try :    
         starting_point = input("What should be the starting point:")
         if isinstance(int(starting_point), int):
-            print("Ok, the program will start at {starting_point}.")
             true_false2 = True 
+            print(f"Ok, the program will start at {starting_point}.")
 except :
-    while not true_false:
+    while not true_false2:
         try:    
             if not isinstance(int(starting_point), int):
                 raise ValueError
             else:
-                true_false = True 
+                true_false2 = True 
+                print(f"Ok, the program will start at {starting_point}.")
         except ValueError:
             print("You have to enter a positive number with no decimals")
             starting_point = input("Enter your number again:")
@@ -20,7 +21,7 @@ true_false = False
 try :    
         repeat_range = input("How many times do you want the program to repeat :")
         if isinstance(int(repeat_range), int):
-            print("Ok, the program will repeat {repeat_range} times.")
+            print(f"Ok, the program will repeat {repeat_range} times.")
             true_false = True 
 except :
     while not true_false:
@@ -29,6 +30,7 @@ except :
                 raise ValueError
             else:
                 true_false = True 
+                print(f"Ok, the program will repeat {repeat_range} times.")
         except ValueError:
             print("You have to enter a positive number with no decimals")
             repeat_range = input("Enter your number again:")
@@ -36,15 +38,14 @@ for i in range(int(repeat_range)):
     current_number = starting_point
     print(current_number)
     while current_number not in verified_set:
-        if (current_number % 2) == 0:
-            current_number = current_number / 2
+        if (int(current_number) % 2) == 0:
+            current_number = int(current_number) // 2
         else:
-            current_number = current_number * 3 + 1
+            current_number = int(current_number) * 3 + 1
 
         print(int(current_number))
         current_set.add(int(current_number))
 
     verified_set.update(current_set)
     print(verified_set)
-    starting_point = starting_point + 1
-        
+    starting_point = int(starting_point) + 1
